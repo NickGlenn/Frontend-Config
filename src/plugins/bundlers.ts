5752b5a97268f47f5811c548fa133474c6a35bcf
Nicholas Glenn <nick@helpfulhuman.com>
2020-02-06 09:28:42 -0800

    Removed unused import

--- src/plugins/bundlers.ts
+++ src/plugins/bundlers.ts
@@ -1,6 +1,5 @@
 import { CreateRollupConfig } from "../rollup";
 import { join } from "path";
-import { pick } from "../utils";
 
 const commonjs = require("@rollup/plugin-commonjs");
 const resolve = require("@rollup/plugin-node-resolve");
